f4c4daa2a7b663ccb212f9f9535c2ca1f1d0b68c
Shivank Singh Baghel <baghelshivank97@gmail.com>
2022-12-25 02:08:41 +0530

    added decrease quantity functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -67,12 +67,88 @@ class CartItem extends React.Component{
 */
 
     increaseQuantity = () => {
-        // console.log("this.state", this.state);
+        /*
+        this.state.qty += 1;
+        console.log("this.state", this.state); 
+        // Increasing quantity like this results in "qty" being updated in the object "this.state" ; but the updated quantity doesn't show up in the UI. The reason being React not knowing that the state has been modified so that it can refresh/re-render the component. 
+        */
+        // In order to make sure that the React gets to know about the updates in the state, we have the function called "setState" which is inherited from the "React.Component" class. We have two ways of using it : - 
+
+        // setState form 1 : via passing an object : Use this form when accessing of previous state isn't required to perform a change on the state. For instance, changing of "qty" does require the previous state but "title" doesn't.
+        this.setState({
+            // qty : this.state.qty + 1  //quantity should ideally be handled in the form 2
+            // title : "Redmi Note 9 Pro" 
+        });
+        // Behind the scenes, React will take this object and will merge it to this.state via shallow merging (only making changes to the modified properties leaving other key value pairs untouched. In this case, title,price&img will remain untouched) and then will re-render our component with the updated state data.
+
+        // setState form 2 : via passing a callback function : Use this form when you require the previous state to perform a change on the state. 
+        this.setState( (prevState) => {
+            return{
+                qty : prevState.qty + 1
+            } 
+        });
+        // The function being passed to the setState function will be called by React internally and meanwhile React will also pass it the previous state as an arguement. That internal function will again return an object that will shallow merge to this.state
+
+        // NOTE : Calling of the setState function always results in re-rendering of the associated component.  
+/*
+    NOTE for setState form 1 : 
+        this.setState({
+            qty : this.state.qty + 3
+        });
+        this.setState({
+            qty : this.state.qty + 4
+        });
+        this.setState({
+            qty : this.state.qty + 1
+        });
+
+         //When we call this.setState (without previous state as a parameter) multiple times inside an event handler function, those all calls will be ignored by the React save by the last one (a process called batching) and thus rendering of the component will only happen once (with only a unit change in the value of "qty" in the above example which is reflective of the change made in the last call). 
+
+*/
+
+/* 
+    NOTE for setState form 2 : 
+        this.setState( (prevState) => {
+            return{
+                qty : prevState.qty + 3
+            } 
+        }); 
+          this.setState( (prevState) => {
+            return{
+                qty : prevState.qty + 2
+            } 
+        }); 
+        this.setState( (prevState) => {
+            return{
+                qty : prevState.qty + 1
+            } 
+        });
+
+        //When we call this.setState (with previous state as a parameter) multiple times inside an event handler function, those all calls will be merged by the React into a single call (a process called batching) and thus rendering of the component will only happen once (but change in the value of "qty" in the above example will be +6, which is an aggregate effect of all the calls made. This happened because all the calls got lined up in a queue and then got executed one by one). 
+*/
+        // console.log(this.state);
+        // Our setState call is asynchronous, i.e., we can't definitively know when a particular call actually finishes. So when we try to print something like this.state as we did above, the value that gets printed wouldn't necessarily be the latest one(in fact could be older one). The value printed could soon become the outdated one, as there might be a setState call in the queue pending to get executed which ones executed at some later time will alter the value of this.state. To tackle this issue, React gives us an option to pass two callback functions to this.setState() function (for both form 1 and form 2), where the second callback fxn gets executed only after the first callback fxn has been executed completely : 
+        /* 
+            this.setState( (prevState) => {
+                return{
+                    qty : prevState.qty + 1
+                } 
+            }, () => {
+                console.log("this.state", this.state);
+            });
+        */
     }
 
     decreaseQuantity = () => {
         // console.log("minus testing");
-
+        this.setState((prevState) => {
+            if(prevState.qty>0) {
+                return {
+                    qty : prevState.qty - 1 
+                }
+            }
+            return;
+        });
     }
 
     deleteItem = () => {
@@ -80,7 +156,9 @@ class CartItem extends React.Component{
 
     }
 
-    render(){
+    render(){  
+        // console.log("render");  //to keep track how many times our component got rendered while experimenting with this.State
+
         // Instead of using this.state.blahblah everytime, we will destructure "this" and make things easier : 
         const {title, price, qty} = this.state ;
         return(
